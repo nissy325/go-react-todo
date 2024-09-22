4e0ea8181d581bfc7f57fd00a010503892e1a143
nissy325 <240noadress+github@gmail.com>
2024-09-22 15:55:20 +0900

    create todo form

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import { Container, Stack } from "@chakra-ui/react";
 import Navber from "./components/Navbar";
+import TodoForm from "./components/TodoForm";
+import TodoList from "./components/TodoList";
 
 function App() {
   return (
     <Stack h="100vh">
       <Navber />
-      <Container></Container>
+      <Container>
+        <TodoForm />
+        <TodoList />
+      </Container>
     </Stack>
   );
 }
